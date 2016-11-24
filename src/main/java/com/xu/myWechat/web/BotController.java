5300c8560d7cab6ae8a46d06bf57bf0c4c9461b9
myherux <heroxu123@gmail.com>
2016-11-24 16:14:46 +0800

    mybatis init

--- src/main/java/com/xu/myWechat/web/BotController.java
+++ src/main/java/com/xu/myWechat/web/BotController.java
@@ -3,8 +3,12 @@ package com.xu.myWechat.web;
 import com.xu.myWechat.common.util.Aes;
 import com.xu.myWechat.common.util.HttpHandler;
 import com.xu.myWechat.common.util.Md5;
+import com.xu.myWechat.mapper.BotXMapper;
+import com.xu.myWechat.pojo.dao.BotX;
 import net.sf.json.JSONObject;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/bot")
 public class BotController {
 
-    private Logger logger = Logger.getLogger(BotController.class);
+    private Logger logger = LogManager.getLogger();
 
+    private final BotXMapper botXMapper;
+
+    @Autowired
+    public BotController(BotXMapper botXMapper) {
+        this.botXMapper = botXMapper;
+    }
 
     @RequestMapping(value = "/doudou", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public String bot(@RequestParam("str") String str) throws Exception {
+    public String doudou(@RequestParam("str") String str) throws Exception {
 
-        logger.info(str);
+        logger.info(str, str);
         //图灵网站上的secret
         String secret = "143859379038ac33";
         //图灵网站上的apiKey
@@ -51,7 +61,14 @@ public class BotController {
         json.put("data", data);
         //请求图灵api
         String result = HttpHandler.post("http://www.tuling123.com/openapi/api", json.toString());
-        System.out.println(result);
+        logger.info(result);
         return result;
+    }
+
+    @RequestMapping(value = "/xuhua", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String xuhua() throws Exception {
+        BotX botX = botXMapper.findById(1);
+        logger.info(botX.getName());
+        return botX.getName();
     }
 }
